276716b67a3b3fdffdd9d504dff544ed34dbaf51
Aditya Vyas <adityavyas@VF327.local>
2025-08-16 15:57:32 +0530

    fix: unnecsarey import removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import {motion} from 'framer-motion'
 import { ShoppingCart, PenTool, BookOpen, Printer } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+
 
 export default function Home() {
   return (
